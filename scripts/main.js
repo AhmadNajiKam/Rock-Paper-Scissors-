30c669e0fefa96bf69e8b211170ba75b7d4badff
AhmadNajiKam <ahmadnajikam@gmail.com>
2025-01-27 22:19:56 +0300

    Implements getHumanChoice function to take input from user

--- scripts/main.js
+++ scripts/main.js
@@ -4,5 +4,21 @@ const getComputerChoice = () => {
   else if (choice > 0.333 && choice <= 0.666) return "paper";
   else return "scissors";
 }
+const getHumanChoice = () => {
+  let choice;
+  while (true) {
+    choice = prompt("Enter your choice: 0.Rock 1.Paper 2.Scissors ? ");
+    if (choice <= 2 && choice >= 0)
+      break;
+  }
+  let decision;
+  switch (Number(choice)) {
+    case 0: decision = "rock"; break;
+    case 1: decision = "paper"; break;
+    case 2: decision = "scissors"; break;
+  }
+  return decision;
+}
 
 console.log(getComputerChoice());
+console.log(getHumanChoice());
