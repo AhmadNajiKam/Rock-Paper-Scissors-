5be46b7d21a5cdb80182f90b9e3d32f344931aeb
AhmadNajiKam <ahmadnajikam@gmail.com>
2025-01-27 18:59:00 +0300

    Implement the game's random function

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,8 @@
-console.log('Hello world');
+const getComputerChoice = () => {
+  const choice = Math.random();
+  if (choice >= 0 && choice <= 0.333) return "rock";
+  else if (choice > 0.333 && choice <= 0.666) return "paper";
+  else return "scissors";
+}
+
+console.log(getComputerChoice());
